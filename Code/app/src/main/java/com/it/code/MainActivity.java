5226391d9afa9972561144063b992ad8cd3800ad
DESKTOP-KKAQ0RK\it <renn12@naver.com>
2017-10-23 13:21:08 +0900

    code

---
+++ Code/app/src/main/java/com/it/code/MainActivity.java
@@ -0,0 +1,46 @@
+package com.it.code;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        LinearLayout container = new LinearLayout(this);
+        container.setOrientation(LinearLayout.VERTICAL);
+
+        Button button1 = new Button(this);
+        button1.setText("button1");
+        container.addView(button1);
+
+        Button button2 = new Button(this);
+        button2.setText("button2");
+        container.addView(button2);
+
+        textView = new TextView(this);
+        textView.setText("text");
+        container.addView(textView);
+
+        setContentView(container);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("button1 click");
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("button2 click");
+            }
+        });
+    }
+}
