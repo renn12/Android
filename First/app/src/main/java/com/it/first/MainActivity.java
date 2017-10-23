a52675f753f593c72f5a2854d362849162ddb41f
DESKTOP-KKAQ0RK\it <renn12@naver.com>
2017-10-23 12:46:42 +0900

    onClick

--- First/app/src/main/java/com/it/first/MainActivity.java
+++ First/app/src/main/java/com/it/first/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
+    Button btninput;
     TextView textView;
 
     @Override
@@ -16,12 +16,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.btninput);
+        btninput = (Button) findViewById(R.id.btninput);
         textView = (TextView) findViewById(R.id.tv01);
 
+
     }
-    public void click(View v){
-        Toast.makeText(getApplicationContext(), "click", Toast.LENGTH_SHORT).show();
+    public void click(View v) {
+        Button button = (Button) v;
+        Toast.makeText(getApplicationContext(), button.getText().toString(), Toast.LENGTH_SHORT).show();
         textView.setText("click");
+        setContentView(R.layout.test_layout);
+    }
+
+    public void click02(View v){
+        Toast.makeText(getApplicationContext(), "button 02 clicked", Toast.LENGTH_SHORT).show();
+        setContentView(R.layout.xml_layout);
+        
+        Button button = (Button) findViewById(R.id.button5);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_main);
+            }
+        });
+    }
+
+    public void click5(View v){
+        switch(v.getId()){
+            case R.id.button2:
+                Toast.makeText(getApplicationContext(), "222", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button4:
+                Toast.makeText(getApplicationContext(), "444", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
