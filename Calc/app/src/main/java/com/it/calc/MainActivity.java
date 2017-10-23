6958f77d13eac8e98c5b2ebe5dab3fc52f45f067
DESKTOP-KKAQ0RK\it <renn12@naver.com>
2017-10-23 15:29:17 +0900

    calc

---
+++ Calc/app/src/main/java/com/it/calc/MainActivity.java
@@ -0,0 +1,33 @@
+package com.it.calc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edNum1, edNum2;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edNum1 = (EditText) findViewById(R.id.edNum1);
+        edNum2 = (EditText) findViewById(R.id.edNum2);
+        textView = (TextView) findViewById(R.id.tvResult);
+    }
+    public void calc(View v){
+        int result = 0;
+        int num1 = Integer.parseInt(edNum1.getText().toString());
+        int num2 = Integer.parseInt(edNum2.getText().toString());
+
+        if(v.getId() == R.id.btnAdd) result = num1 + num2;
+        else if(v.getId() == R.id.btnSub) result = num1 - num2;
+        else if(v.getId() == R.id.btnMul) result = num1 * num2;
+        else if(v.getId() == R.id.btnDiv) result = num1 / num2;
+        textView.setText(result+"");
+    }
+}
